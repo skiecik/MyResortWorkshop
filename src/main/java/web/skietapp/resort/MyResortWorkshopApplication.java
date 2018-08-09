8a7c259d4c17a2558fa39ecac9bb696156f3723b
skiecik <skiecik@skietcode.hub.pl>
2018-08-09 13:47:15 +0200

    first commit

---
+++ src/main/java/web/skietapp/resort/MyResortWorkshopApplication.java
@@ -0,0 +1,12 @@
+package web.skietapp.resort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResortWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResortWorkshopApplication.class, args);
+	}
+}
